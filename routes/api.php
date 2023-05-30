47b65f87d771a3b46bbcc114d3a2efeed1024e26
Luksona2006 <luka.bakuridze@redberry.ge>
2023-05-30 17:18:53 +0400

    refactor: import controllers and define them in routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\UserController;
+use App\Http\Controllers\LocalizationController;
+use App\Http\Controllers\SocialiteController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,23 +22,23 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('locale', [App\Http\Controllers\LocalizationController::class, 'setLocale']);
+Route::post('locale', [LocalizationController::class, 'setLocale']);
 
 Route::group(['middleware' => 'guest:sanctum'], function () {
-    Route::post('login', [App\Http\Controllers\API\AuthController::class, 'login'])->name('login');
-    Route::post('signup', [App\Http\Controllers\API\AuthController::class, 'register'])->name('signup.register');
-    Route::get('verify/{token}', [App\Http\Controllers\API\AuthController::class,'verifyEmail'])->name('verify.verifyEmail');
-    Route::post('forgot-password', [App\Http\Controllers\API\AuthController::class,'sendPasswordResetRequest'])->name('forgot-password.send-password-reset-request');
-    Route::post('reset-password/{token}', [App\Http\Controllers\API\AuthController::class,'resetPassword'])->name('reset-password.resetPassword');
+    Route::post('login', [AuthController::class, 'login'])->name('login');
+    Route::post('signup', [AuthController::class, 'register'])->name('signup.register');
+    Route::get('verify/{token}', [AuthController::class, 'verifyEmail'])->name('verify.verifyEmail');
+    Route::post('forgot-password', [AuthController::class, 'sendPasswordResetRequest'])->name('forgot-password.send-password-reset-request');
+    Route::post('reset-password/{token}', [AuthController::class, 'resetPassword'])->name('reset-password.resetPassword');
 });
 
 Route::group(['middleware' => 'auth:sanctum'], function () {
-    Route::post('logout', [App\Http\Controllers\API\AuthController::class, 'logout'])->name('logout');
+    Route::post('logout', [AuthController::class, 'logout'])->name('logout');
 
-    Route::get('users/{token}', [App\Http\Controllers\API\UserController::class, 'show'])->name('users.show');
-    Route::put('users/{token}', [App\Http\Controllers\API\UserController::class, 'update'])->name('users.update'); // Not using it yet
+    Route::get('users/{token}', [UserController::class, 'show'])->name('users.show');
+    Route::put('users/{token}', [UserController::class, 'update'])->name('users.update'); // Not using it yet
 });
 
-Route::get('/auth/google/redirect', [App\Http\Controllers\SocialiteController::class, 'socialiteRedirect'])->name('auth-google.socialiteRedirect');
-Route::get('/auth/google/callback', [App\Http\Controllers\SocialiteController::class, 'socialiteCreateUser'])->name('auth-google.socialiteCreateUser');
+Route::get('/auth/google/redirect', [SocialiteController::class, 'socialiteRedirect'])->name('auth-google.socialiteRedirect');
+Route::get('/auth/google/callback', [SocialiteController::class, 'socialiteCreateUser'])->name('auth-google.socialiteCreateUser');
 ;
