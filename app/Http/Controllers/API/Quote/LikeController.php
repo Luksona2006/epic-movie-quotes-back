75aaa0e7005b6c7025aa995f79c8b6d2b33674d3
Luksona2006 <luka.bakuridze@redberry.ge>
2023-07-05 16:41:46 +0400

    refactor: change isOwnQuote parameter to userId for LikeQuote event, do not create like notification if user liked it once

--- app/Http/Controllers/API/Quote/LikeController.php
+++ app/Http/Controllers/API/Quote/LikeController.php
@@ -15,43 +15,43 @@ class LikeController extends Controller
 {
     public function like(Quote $quote, Request $request): JsonResponse
     {
-        $user = auth()->user();
+        $userId= auth()->id();
 
         $likes = QuoteUser::where('quote_id', $quote->id)->get()->toArray();
         $likesSum = count($likes);
-        $liked = QuoteUser::where('quote_id', $quote->id)->where('user_id', $user->id)->count() > 0;
-        if($request->liked !== null) {
-            if($request->liked === true) {
-                QuoteUser::create([
-                    'user_id' => $user->id,
-                    'quote_id' => $quote->id
-                ]);
+        $likedBefore = QuoteUser::where('quote_id', $quote->id)->where('user_id', $userId)->count() > 0;
 
-                $likesSum = $likesSum + 1;
-                $liked = true;
-            }
+        if($request->liked === true) {
+            QuoteUser::create([
+                'user_id' => $userId,
+                'quote_id' => $quote->id
+            ]);
 
-            if($request->liked === false) {
-                QuoteUser::where([
-                    ['user_id', $user->id],
-                    ['quote_id', $quote->id]
-                ])->first()->delete();
-
-                $likesSum = $likesSum - 1;
-                $liked = false;
-            }
-
-            $isOwnQuote = $user->id === $quote->id;
-
-            if(!$isOwnQuote) {
-                $notification = Notification::create(['from_user' => $user->id, 'to_user' => $quote->user_id, 'quote_id' => $quote->id, 'type' => 'like']);
-                $notificationFullData = [...$notification->toArray()];
-                $notificationFullData['user'] = $user;
-                event(new RecieveNotification($quote->user_id, $notificationFullData));
-            }
-
-            event(new LikeQuote($quote->id, $likesSum, $isOwnQuote));
+            $likesSum = $likesSum + 1;
+            $liked = true;
         }
+
+        if($request->liked === false) {
+            QuoteUser::where([
+                ['user_id', $userId],
+                ['quote_id', $quote->id]
+            ])->first()->delete();
+
+            $likesSum = $likesSum - 1;
+            $liked = false;
+        }
+
+        $isOwnQuote = $userId === $quote->id;
+
+        if(!$isOwnQuote || !$likedBefore) {
+            $notification = Notification::create(['from_user' => $userId, 'to_user' => $quote->user_id, 'quote_id' => $quote->id, 'type' => 'like']);
+            $notificationFullData = [...$notification->toArray()];
+            $notificationFullData['user'] = auth()->user();
+            event(new RecieveNotification($quote->user_id, $notificationFullData));
+        }
+
+        event(new LikeQuote($quote->id, $likesSum, $userId));
+
 
         return response()->json(['liked' => $liked]);
     }
